0c1ba4307a4b9a81caa42187e50ea895f51cbb84
funnyddq <funnyddq@gmail.com>
2021-11-14 11:06:29 +0800

    Home

---
+++ src/js/my.js
@@ -0,0 +1,5 @@
+var a = [1, 2, 3];
+
+console.log(a.__proto__ === Array.prototype);
+console.log(Array.prototype.isPrototypeOf(a));
+
---
+++ src/js/test.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+    console.log(typeof a);
+    console.log(typeof b);
+}
+
+var undefined = 2;
+add(2, undefined);
