960b3a5c7aa2edf7a177fe7d6810656466881686
funnyddq <funnyddq@gmail.com>
2021-11-18 17:16:16 +0800

    Home

--- src/js/my.js
+++ src/js/my.js
@@ -1,2 +1,9 @@
-var doc = $('document');
-console.log(doc);
+$(function() {
+    $('p').append('<p id="append">append</p>');
+    var mynode = $('#append');
+    mynode.attr('nonce', 'xxx-yyy');
+// $.globalEval('console.log(\'*****OK******\');', {
+//     nonce:"nonce-2726c7f26c"
+// });
+    $.globalEval('console.log(\'*****OK******\');', mynode);
+});
